8363f1fdb75c684577af3213c13218603d9e48bb
Erin Green <erin.y.green@gmail.com>
2019-11-20 21:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(json => renderBooks(json));
 }
 
-const json = fetchBooks()
 
 function renderBooks(json) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  json.forEach(book => 
+    {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
@@ -16,5 +16,5 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  renderBooks()
+  fetchBooks()
 })
